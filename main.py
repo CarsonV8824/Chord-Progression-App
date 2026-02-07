0f1cfb979cac95a61d2677f1e4933221c1b2fe3c
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-02-07 12:53:03 -0600

    fixed file path

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from app.window import MainWindow
-
 from PyQt6.QtGui import QIcon
-
 import os
+
+def resource_path(rel_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, rel_path)
 
 def main():
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon(os.path.join(os.path.dirname(__file__), "styles", "logo.png")))
+    app.setWindowIcon(QIcon(resource_path("styles/logo.png")))
     window = MainWindow()
-    window.setWindowIcon(QIcon(os.path.join(os.path.dirname(__file__), "styles", "logo.png")))
+    window.setWindowIcon(QIcon(resource_path("styles/logo.png")))
     window.setObjectName("main_window")
 
-    stylesheet_path = os.path.join(os.path.dirname(__file__), "styles", "style.css")
+    stylesheet_path = resource_path("styles/style.css")
 
     with open(stylesheet_path, "r") as f:
         style = f.read()
